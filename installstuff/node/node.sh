e469b55b5bd89c2618347c49fdad601b7739a100
root <kim.christensen@snow.edu>
2017-01-22 06:53:45 +0000

    change to node install script

--- installstuff/node/node.sh
+++ installstuff/node/node.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-a-centos-7-server
+
 wget http://nodejs.org/dist/v6.9.4/node-v6.9.4-linux-x64.tar.gz
 
 sudo tar --strip-components 1 -xzvf node-v* -C /usr/local
